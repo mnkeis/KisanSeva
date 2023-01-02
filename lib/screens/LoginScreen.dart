09626dc15a879ca2ee0e5d5da3d0ab995c565992
Mauro Casse <mnkeis@gmail.com>
2023-01-02 19:14:41 -0300

    Stuck on login screen

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -167,142 +167,134 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 70,
               ),
               _topheader(),
-              Expanded(
-                  child: Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.only(top: 32),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(40),
-                          topRight: Radius.circular(40),
-                        ),
-                        color: Colors.grey[50],
-                      ),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                      child: SingleChildScrollView(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                            SizedBox(height: 40),
-                            _labelText(('Phone Number')),
-                            Padding(
-                                padding:
-                                    EdgeInsets.only(left: 25.0, right: 25.0),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.phone,
-                                  decoration: InputDecoration(
-                                    hintText: AppLocalizations.of(context)
-                                        ?.translate('Enter your phone number'),
-                                    prefixText: '+91',
-                                    prefixIcon: Icon(Icons.phone),
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide:
-                                            BorderSide(color: Colors.grey)),
-                                    border: OutlineInputBorder(),
-                                  ),
-                                  onChanged: (val) {
-                                    setState(() {
-                                      this.phoneNo = '+91' + val;
-                                    });
-                                  },
-                                )),
-                            codeSent
-                                ? Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                        SizedBox(height: 40),
-                                        _labelText("Enter OTP"),
-                                        Padding(
-                                            padding: EdgeInsets.only(
-                                                left: 25.0, right: 25.0),
-                                            child: TextFormField(
-                                              keyboardType: TextInputType.phone,
-                                              decoration: InputDecoration(
-                                                hintText: AppLocalizations.of(
-                                                        context)
+              Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 32),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                    color: Colors.grey[50],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: SingleChildScrollView(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                        SizedBox(height: 40),
+                        _labelText(('Phone Number')),
+                        Padding(
+                            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                            child: TextFormField(
+                              keyboardType: TextInputType.phone,
+                              decoration: InputDecoration(
+                                hintText: AppLocalizations.of(context)
+                                    ?.translate('Enter your phone number'),
+                                prefixText: '+91',
+                                prefixIcon: Icon(Icons.phone),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey)),
+                                border: OutlineInputBorder(),
+                              ),
+                              onChanged: (val) {
+                                setState(() {
+                                  this.phoneNo = '+91' + val;
+                                });
+                              },
+                            )),
+                        codeSent
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                    SizedBox(height: 40),
+                                    _labelText("Enter OTP"),
+                                    Padding(
+                                        padding: EdgeInsets.only(
+                                            left: 25.0, right: 25.0),
+                                        child: TextFormField(
+                                          keyboardType: TextInputType.phone,
+                                          decoration: InputDecoration(
+                                            hintText:
+                                                AppLocalizations.of(context)
                                                     ?.translate('Enter OTP'),
-                                                prefixIcon: Icon(Icons.vpn_key),
-                                                enabledBorder:
-                                                    OutlineInputBorder(
-                                                        borderSide: BorderSide(
-                                                            color:
-                                                                Colors.grey)),
-                                                border: OutlineInputBorder(),
-                                              ),
-                                              onChanged: (val) {
-                                                setState(() {
-                                                  this.smsCode = val;
-                                                });
-                                              },
-                                            ))
-                                      ])
-                                : Container(),
-                            SizedBox(height: 20),
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                height: 46,
-                                width: 160,
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    AuthService()
-                                        .savePhoneNumber(this.phoneNo!);
-                                    codeSent
-                                        ? AuthService().signInWithOTP(
-                                            smsCode, verificationId)
-                                        : verifyPhone(phoneNo);
-
-                                    saveData();
-                                  },
-                                  child: codeSent
-                                      ? Text(
-                                          AppLocalizations.of(context)
-                                                  ?.translate('Continue') ??
-                                              'Continue',
-                                          style: TextStyle(fontSize: 20),
-                                        )
-                                      : Text(
-                                          AppLocalizations.of(context)
-                                                  ?.translate('Verify') ??
-                                              'Verify',
-                                          style: TextStyle(
-                                            fontSize: 20,
-                                            color: Colors.white,
-                                            backgroundColor: Color(0XFF303f9f),
+                                            prefixIcon: Icon(Icons.vpn_key),
+                                            enabledBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.grey)),
+                                            border: OutlineInputBorder(),
                                           ),
-                                          // shape: RoundedRectangleBorder(
-                                          //   borderRadius:
-                                          //       BorderRadius.circular(30.0),
-                                          // ),
-                                        ),
-                                ),
-                              ),
+                                          onChanged: (val) {
+                                            setState(() {
+                                              this.smsCode = val;
+                                            });
+                                          },
+                                        ))
+                                  ])
+                            : Container(),
+                        SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Container(
+                            height: 46,
+                            width: 160,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                AuthService().savePhoneNumber(this.phoneNo!);
+                                codeSent
+                                    ? AuthService()
+                                        .signInWithOTP(smsCode, verificationId)
+                                    : verifyPhone(phoneNo);
+
+                                saveData();
+                              },
+                              child: codeSent
+                                  ? Text(
+                                      AppLocalizations.of(context)
+                                              ?.translate('Continue') ??
+                                          'Continue',
+                                      style: TextStyle(fontSize: 20),
+                                    )
+                                  : Text(
+                                      AppLocalizations.of(context)
+                                              ?.translate('Verify') ??
+                                          'Verify',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        backgroundColor: Color(0XFF303f9f),
+                                      ),
+                                      // shape: RoundedRectangleBorder(
+                                      //   borderRadius:
+                                      //       BorderRadius.circular(30.0),
+                                      // ),
+                                    ),
                             ),
-                            SizedBox(height: 12),
-                            Align(
-                              alignment: Alignment(0.9, -0.9),
-                              child: DropdownButton(
-                                underline: SizedBox(),
-                                icon: Icon(Icons.language),
-                                iconSize: 40,
-                                items: Language.languageList()
-                                    .map<DropdownMenuItem>(
-                                        (lang) => DropdownMenuItem(
-                                              child: Text(lang.name ?? 'EN'),
-                                              value: lang,
-                                            ))
-                                    .toList(),
-                                onChanged: (language) {
-                                  _changeLanguage(language);
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 18),
-                          ]))))
+                          ),
+                        ),
+                        SizedBox(height: 12),
+                        Align(
+                          alignment: Alignment(0.9, -0.9),
+                          child: DropdownButton(
+                            underline: SizedBox(),
+                            icon: Icon(Icons.language),
+                            iconSize: 40,
+                            items: Language.languageList()
+                                .map<DropdownMenuItem>(
+                                    (lang) => DropdownMenuItem(
+                                          child: Text(lang.name ?? 'EN'),
+                                          value: lang,
+                                        ))
+                                .toList(),
+                            onChanged: (language) {
+                              _changeLanguage(language);
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 18),
+                      ])))
             ]))));
   }
 
@@ -340,6 +332,7 @@ class _LoginScreenState extends State<LoginScreen> {
     }
 */
   Future<void> verifyPhone(phoneNo) async {
+    print(phoneNo);
     final PhoneVerificationCompleted verified = (AuthCredential authResult) {
       AuthService().signIn(authResult);
     };
